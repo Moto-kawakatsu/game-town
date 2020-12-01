e4d2bbd8afb46ba3bbe28a6f6dcdd1cd4bdb61c0
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-01 11:29:01 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   get 'games/index'
  root to: "games#index"
+ get 'games/block_break' => 'games#block_break'
+ get 'games/osero' => 'games#osero'
+ get 'games/tetris' => 'games#tetris'
+ resources :games
 end
