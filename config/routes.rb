dbe9ae4d2830abc0089571dab5e0d02eb636685b
Moto-kawakatsu <kawakatu1227@gmail.com>
2020-12-08 21:15:08 +0900

    各ゲームのルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
  get 'games/block_break' => 'games#block_break'
  get 'games/osero' => 'games#osero'
  get 'games/tetris' => 'games#tetris'
+ get 'games/osero' => 'games#osero'
+ get 'games/snake' => 'games#snake'
+ get 'games/jump' => 'games#jump'
  resources :games
 end
